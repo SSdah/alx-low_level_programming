e042cfff05dd56748025b68696039f43c4665c90
SSdah <113689182+SSdah@users.noreply.github.com>
2023-02-17 21:02:41 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-/***
- * main -Entry point
- *
- * return:always 0 (success)
- */
-int main(void)
-{
-	int a;
-        long int b;
-	long long int c;
-	char d;
-        float f;
-	
-	printf("Size a of int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
-        return (0);
+# inclure  < stdio.h >
 
+/* *
+* principal - Point d'entrée
+*
+* Retour : Toujours 0 (Succès)
+ */
+int  principal ( vide )
+{
+	int un ;
+	 entier long b ;
+	 entier long long  c ;
+	caractère d ;
+	flottant f ;
+
+	printf ( " Taille d'un caractère : %lu octet(s) \n " , ( unsigned  long ) sizeof (d));
+	printf ( " Taille d'un int : %lu octet(s) \n " , ( unsigned  long ) sizeof (a));
+	printf ( " Taille d'un entier long : %lu octet(s) \n " , ( unsigned  long ) sizeof (b));
+	printf ( " Taille d'un entier long long : %lu octet(s) \n " , ( long non signé  ) sizeof (c));
+	printf ( " Taille d'un flottant : %lu octet(s) \n " , ( unsigned  long ) sizeof (f));
+	retour ( 0 );
 }
